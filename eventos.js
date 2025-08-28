bc3c5c263bea3494004b1dd5de62552322dfa24c
Luis Nunez <luis.nunez@estudiantes.utec.edu.uy>
2025-08-28 02:58:26 -0300

    Solucion de la doble alerta

--- eventos.js
+++ eventos.js
@@ -1,3 +1,14 @@
+let btnPresionado = false
+
+document.getElementById("btn").addEventListener("click", function(){
+    btnPresionado = true
+});
+
 document.getElementById("div").addEventListener("click", function(){
-    alert("Hola! Soy el div");
+    if (btnPresionado) {
+        alert("Hola!");
+        btnPresionado = false
+    }else {
+        alert("Hola! Soy el div");
+    }
 });
